962384436f69a8202f13dcc2cfff949f04b1e5ba
Bryan Vi <bryanhvi99@gmail.com>
2020-08-12 12:37:28 -0400

    gitignore should work

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ import webbrowser
 import spotipy.util as util
 from json.decoder import JSONDecodeError
 
-# TODO: set environment variables USERNAME, CLIENT_ID, CLIENT_SECRET, REDIRECT_URI, ADMIN_EMAIL, ADMIN_EMAIL_PASSWORD
+# set environment variables USERNAME, CLIENT_ID, CLIENT_SECRET, REDIRECT_URI, ADMIN_EMAIL, ADMIN_EMAIL_PASSWORD
 
 username = os.getenv('USERNAME')
 client_id = os.getenv('CLIENT_ID')
@@ -17,15 +17,19 @@ redirect_uri = os.getenv('REDIRECT_URI')
 admin_email = os.getenv('ADMIN_EMAIL')
 admin_email_password = os.getenv('ADMIN_EMAIL_PASSWORD)
 
-
-scope = 'playlist-modify-private playlist-modify-public user-follow-read ugc-image-upload'
-
 token = util.prompt_for_user_token(username=username, scope=scope, client_id=client_id, client_secret=client_secret, redirect_uri=redirect_uri)
+
+#instantiate spotify API token
 sp = spotipy.Spotify(token)
 
 files_used = []
-
 def cleanDirectory(files_used):
+    '''
+    Delete unused text files in directory
+
+    parameters: list of files used
+    return: None
+    '''
     directory = 'C:/Users/bryan/Documents/vscode projects/spotify_development'
     for filename in os.listdir(directory):
         if filename.endswith(".txt"):
@@ -33,16 +37,18 @@ def cleanDirectory(files_used):
                 os.remove(directory + '/' + filename)
 
 
-def readFile():
+def readFile(file):
     '''
-    function opens user_file.txt and returns list of plahylist URIs
-    '''
-    file = open(r'user_file.txt')
+    Opens user_file.txt and returns list of plahylist URIs
 
+    Parameters: IO TextWrapper file 'user.txt'
+    Return: dictionary of users as keys and lists of playlist uris as respective values
+    '''
     user_dict = {}
     
     user = ''
 
+    # iterate through user_file.txt and assign a list value of all uris to each user 
     for line in file:
         if line[0] == '#':
             continue
@@ -60,29 +66,33 @@ def readFile():
 
 def updatePlaylists(uri_list):
     '''
-    1. read through uris
-    2. for each playlist, use current uri to get playlist name
-    3. open the 2 files with the playlist name ('x-current.txt / 'x-last_update.txt'), otherwise create new files
-    4. update current.txt, add to current={set}
-    5. iterate through last_updat.txt, last_updat={set} 
-    6. compare sets, add new songs to message.txt
+    Read throught the user's playlist uris. If the playlist uri was recently added to the list of
+    playlists to get updated on, create two text files of the playlist tracks. If the files have already been created, 
+    compare the two text files to check what songs have been added or deleted.
+
+    Paramters: a list of plalist uris
+    Return: a dictionary holding all the songs that were added since the last update and the songs that were deleted
     '''
     if uri_list:
 
         update_dict = {}
 
+        
         for index in range(0, len(uri_list)):
-            
+            # for each playlist, use current uri to get playlist name
             playlist_items = sp.playlist_tracks(uri_list[index], fields='items', limit=100)
             name = sp.playlist(uri_list[index])['name']
             
+            # create sets to compare later on
             old_set = set()
             new_set = set()
             
+            # attempt to open playlist track files if the playlist had already had updates made on it
             try:
                 last_update = open(name + ' (last_update).txt', 'r', encoding='utf-8')
-                files_used.append(last_update.name)
-                for track in last_update:
+                files_used.append(last_update.name)     #file is being used
+                
+                for track in last_update:           # add tracks from previous update to old_set
                     if '\n' in track:
                         old_set.add(track[0:-1])
                     else:
@@ -91,47 +101,53 @@ def updatePlaylists(uri_list):
                 
                 current = open(name + ' (current).txt', 'w', encoding='utf-8')
                 files_used.append(current.name)
-                for song in playlist_items['items']:      # for song in list
+                for song in playlist_items['items']:      # for song in playlist, add songs in current plalist to new_set
                     track = song['track']['name'] + '  ―  ' + song['track']['artists'][0]['name']  #type str (ex. Super Rich Kids by Frank Ocean)
                     if '\n' in track:
                         new_set.add(track[0:-1])
                     else:
                         new_set.add(track)
                     
-                    current.write(track)
+                    current.write(track)        # update text file of songs in current playlist
                     current.write('\n')
                 current.close()
 
                 new_songs = []
                 deleted_songs = []
 
-                to_add = new_set - old_set
+                to_add = new_set - old_set      # if there are songs in new_set that aren't in old_set, add to list of new songs
                 for song in to_add:
                     new_songs.append(song)
-                new_songs.sort(reverse=True, key=str.casefold)
+                
+                new_songs.sort(reverse=True, key=str.casefold)      #sort songs list in reverse alphabetical order
 
-                to_remove = old_set - new_set
+                to_remove = old_set - new_set   #if there are songs in old_set that aren't in new_set, add to list of deleted_songs
                 for song in to_remove:
                     deleted_songs.append(song)
-                deleted_songs.sort(reverse=True, key=str.casefold)
+                
+                deleted_songs.sort(reverse=True, key=str.casefold)      #sort songs list in reverse alphabetical order
 
 
-                last_update = open(name + ' (last_update).txt', 'w', encoding='utf-8')
+                last_update = open(name + ' (last_update).txt', 'w', encoding='utf-8')      # update last_update.txt to be the same as current update
                 current = open(name + ' (current).txt', 'r', encoding='utf-8')
                 for song in current:      # for song in list
                     last_update.write(song)
                 last_update.close()
 
-                update_dict[name] = {'new_songs': new_songs, 'deleted_songs': deleted_songs}
+                update_dict[name] = {'new_songs': new_songs, 'deleted_songs': deleted_songs}        #create dictionary to return
 
 
-            except FileNotFoundError:                       # new playlist, so create and update both playlist files and do nothing
+            except FileNotFoundError:              # new playlist, so create and update both playlist files and do nothing else
                 
                 last_update = open(name + ' (last_update).txt', 'w', encoding='utf-8')
                 current = open(name + ' (current).txt', 'w', encoding='utf-8')
-                files_used.append(last_update.name)
+
+                files_used.append(last_update.name)     #files are being user, so don't delete
                 files_used.append(current.name)
+
                 for song in playlist_items['items']:      # for song in list
+
+                    # create two files of all songs in playlist to compare for next update
                     last_update.write(song['track']['name'] + '  ―  ' + song['track']['artists'][0]['name'])
                     last_update.write('\n')
                     current.write(song['track']['name'] + '  ―  ' + song['track']['artists'][0]['name'])
@@ -140,41 +156,49 @@ def updatePlaylists(uri_list):
                 last_update.close()
                 current.close()
 
-                print('new files were created for {}'.format(name), file=sys.stderr)
-                update_dict[name] = {'new_songs': '(new playlist)', 'deleted_songs': '(new playlist)'}
+                update_dict[name] = {'new_songs': '(new playlist)', 'deleted_songs': '(new playlist)'}      #create dictionary to return, but with no songs
 
         return update_dict
-    
-    else:
-        print('\nThis user has no playlists selected\n', file=sys.stderr)
+
 
 
 def user_message(update_dict, uri_list):
+    '''
+    Writes neatly formatted update message to send in an email
 
+    Parameters: dictionary of playlists to update, list of uris to get playlist names
+    Return: string message to send in email
+    '''
+    #get playlist names
     playlist_names = []
     for uri in uri_list:
         playlist_names.append(sp.playlist(uri)['name'])
-    message = "Subject: Here's your update for this week.\n\n\n"
+
+    message = "Subject: Here's your update for this week.\n\n\n"        # begin message
 
     for name in playlist_names:
         message += '[ Update for ' + name + ': ]\n\n'
+
+        # check to see if playlist has just been added to user_file.txt
         if update_dict[name]['new_songs'] == '(new playlist)':
             message += ('     Playlist has been added to the Updater! You will now get updates for this playlist.\n\n\n')
             continue
+
+
         new_songs = update_dict[name]['new_songs']
         deleted_songs = update_dict[name]['deleted_songs']
 
+        # write to message the songs that have been added since the last update
         if new_songs:
             message += '+++++Songs Added+++++\n\n'
-    
             while len(new_songs) != 0:
                 message += '+ ' + new_songs[-1] + '\n'
                 new_songs.pop()
             message += '\n'
-
-        else:
+        else:               # if no songs have been added, tell user
             message += '~ No new songs were added. ~\n\n\n'
         
+        # write to message the songs that have been deleted since the last update
         if deleted_songs:
             message += '\n'
             message += '-----Songs Removed-----\n\n'
@@ -196,21 +220,25 @@ def sendEmail(user_email, message):
     
     port = 465  # For SSL
     smtp_server = "smtp.gmail.com"
-    sender_email = "spotifyplaylistupdater@gmail.com"  # Enter your address
+    sender_email = admin_email  # Enter your address
     receiver_email = user_email  # Enter receiver address
-    password = "thierryHenry14#"
-    
-    context = ssl.create_default_context()
-    with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
-        server.login(sender_email, password)
+    password = admin_email_password
 
-        server.sendmail(sender_email, receiver_email, message.encode('utf-8'))
+    
+    context = ssl.create_default_context()      #encrypt message
+    with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
+        
+        server.login(sender_email, password)        # login to email
+
+        server.sendmail(sender_email, receiver_email, message.encode('utf-8'))      #send email with utf-8 encodeding to account for speical charecters / emojis
         print("sent email", file=sys.stderr)
 
 
 
 def main():
-    user_dict = readFile()
+
+    file = open(r'user_file.txt')
+    user_dict = readFile(file)          # get users and the playlists they want to be updates on
     
     for user_email, uri_list in user_dict.items():
         print(user_email)
