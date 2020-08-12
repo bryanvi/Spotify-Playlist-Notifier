940d7b26e41b84385f567c72b05a5c7e58902d3b
Bryan Vi <bryanhvi99@gmail.com>
2020-08-12 11:27:19 -0400

    Delete run.bat

--- run.bat
+++
@@ -1,8 +0,0 @@
-setlocal
-set ACCOUNT_OWNER=bryanvi
-set CLIENT_ID=3e23d0e194824d46aa6a3a8a821ea72f
-set CLIENT_SECRET=61703860a06941d588248515ecad9452
-set REDIRECT_URI=http://localhost:8080/
-
-set SENDER_EMAIL=spotifyplaylistupdater@gmail.com
-set SENDER_EMAIL_PASSWORD=thierryHenry14#
