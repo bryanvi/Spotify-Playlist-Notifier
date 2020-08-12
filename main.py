fb0bd2b76be5a95a838cdbee5dae014d0d4c5290
Bryan Vi <bryanhvi99@gmail.com>
2020-08-12 12:03:43 -0400

    Update main.py

--- main.py
+++ main.py
@@ -196,9 +196,9 @@ def sendEmail(user_email, message):
     
     port = 465  # For SSL
     smtp_server = "smtp.gmail.com"
-    sender_email = "spotifyplaylistupdater@gmail.com"  # Enter your address
+    sender_email = ADMIN_EMAIL  # Enter your address
     receiver_email = user_email  # Enter receiver address
-    password = "thierryHenry14#"
+    password = ADMIN_EMAIL_PASSWORD
     
     context = ssl.create_default_context()
     with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
